b9517372e5ec238c0af1f22c14b8ef139bdd4ea1
efiera <tur99267@temple.edu>
2024-12-03 02:11:59 -0500

    cors

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
+        cors: false,
         proxy: {
             '/spotify': {
                 target: 'https://accounts.spotify.com',
