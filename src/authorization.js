49eebe7bc57b060658b29faf05cbbc475c6f3c58
efiera <tur99267@temple.edu>
2024-12-03 00:08:23 -0500

    callback

--- src/authorization.js
+++ src/authorization.js
@@ -1,7 +1,8 @@
 import * as userData from './user_data.js';
 
 export const clientID = "9f79956a03b04bcfb5df0ff2a5a78059";
-export const redirectURI = "https://localhost:5173/callback";
+//export const redirectURI = "https://localhost:5173/callback";
+export const redirectURI = "https://musicmatcherdb.web.app/callback";
 const scope = "user-read-private user-read-email user-top-read user-follow-modify user-modify-playback-state";
 const authURL = new URL("https://accounts.spotify.com/authorize");
 
